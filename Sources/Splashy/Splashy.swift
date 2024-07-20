2f695099a016691164a80c6f6b7f5662d95c0980
N3v1 <n3v1010@gmail.com>
2024-07-21 00:01:58 +0200

    (feat): Add necessary files

--- Sources/Splashy/Splashy.swift
+++ Sources/Splashy/Splashy.swift
@@ -1,2 +1,26 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+//===-- Splashy/Splashy.swift - Main ----------------------------  -*- swift -*-===//
+//                                                                                 //
+// This source file is part of the Splashy - UnsplashAPIWrapper                    //
+// open source project.                                                            //
+//                                                                                 //
+// Copyright (c) 2015 - 2024 Unsplash - All rights reserved. (API)                 //
+// Copyright (c) 2024 Nevio Hirani and the Splashy project authors - All           //
+//               rights reserved. (Splashy)                                        //
+//                                                                                 //
+// See CONTRIBUTORS.txt for the list of Splashy project authors                    //
+//                                                                                 //
+// You may not use this file except in compliance with the License.                //
+// You may obtain a copy of the License at                                         //
+//                                                                                 //
+//      http://www.apache.org/licenses/LICENSE-2.0                                 //
+//                                                                                 //
+// Unless required by applicable law or agreed to in writing, software             //
+// distributed under the License is distributed on an "AS IS" BASIS,               //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.        //
+// See the License for the specific language governing permissions and             //
+// limitations under the License.                                                  //
+//                                                                                 //
+//===---------------------------------------------------------------------------===//
